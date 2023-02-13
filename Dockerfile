1cdf2f19e16bf77e59fd033453b55f4b39603775
Davide D'Innocente <D.DInnocente.esterno@almaviva.it>
2023-02-13 02:14:20 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,30 @@
-FROM golang:1.9 
+FROM golang:1.19
 
-ENV GOPATH=/app
-ENV WORKPATH=/app/src
 
-COPY src $WORKPATH
 
-WORKDIR $WORKPATH
+WORKDIR /usr/src/app
 
-RUN go get .
+# pre-copy/cache go.mod for pre-downloading dependencies and only redownloading them in subsequent builds if they change
+#COPY go.mod go.sum ./
 
-RUN go build -o application .
+COPY src .
 
+#WORKDIR /usr/src/app/src
+
+RUN go mod download && go mod verify
+
+
+RUN go build -v 
+#RUN go build -v -o /usr/local/bin/app ./...
+#RUN go build -v -o $WORKDIR ./...
+
+ENV PATH=/usr/src/app
 EXPOSE 8080
 
-CMD ["./application"]
+CMD ["golang-users"]
+
+
+#You can then build and run the Docker image:
+
+#$ docker build -t users-image:1 .
+#$ docker run -it -p 8080:8080 -e mysql_users_host=172.17.0.1 --rm --name users-container users-image:1 
--- src/datasources/mysql/users_db/users_db.go
+++ src/datasources/mysql/users_db/users_db.go
@@ -3,12 +3,12 @@ package users_db
 import (
 	"database/sql"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
 	"github.com/Davide202/golang-users/utils/logger"
+	"github.com/go-sql-driver/mysql"
 	"log"
-	_ "os"
+	"os"
 )
-
+const mysqlUsersHost = "mysql_users_host"
 /*
 const (
 
@@ -21,25 +21,38 @@ const (
 */
 var (
 	Client *sql.DB
+	
 	/*
 		username = os.Getenv(mysqlUsersUsername)
 		password = os.Getenv(mysqlUsersPassword)
-		host     = os.Getenv(mysqlUsersHost)
+		host     = os.Getenv(mysqlUsersHost) "172.17.0.1"(container)
 		schema   = os.Getenv(mysqlUsersSchema)
 	*/
 )
 
 func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"root", "admin", "localhost", "db_users",
-	)
+
+	host, maybe := os.LookupEnv(mysqlUsersHost)
+	var dataSourceName string 
+	if maybe {
+		dataSourceName = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "admin", host, "db_users")
+	}else{
+		dataSourceName = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "admin", "localhost", "db_users")
+	}
+
+	
+
+	//fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8","root", "admin", "localhost", "db_users",)
+	log.Println("database connection: " + dataSourceName)
 	var err error
 	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
-		panic(err)
+		//panic(err)
+		log.Println("database connection: " + err.Error())
 	}
 	if err = Client.Ping(); err != nil {
-		panic(err)
+		//panic(err)
+		log.Println("database connection: " + err.Error())
 	}
 
 	mysql.SetLogger(logger.GetLogger())
